1b8675d44e6cc7899dd9d006a309d68ff0690917
unknown <marq.java@gmail.com>
2023-08-03 23:42:02 -0500

    First Text Editor build with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { NgxWigModule } from 'ngx-wig';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
